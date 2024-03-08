ce1b032214f1e1ddf0bf943b89f37dce5e68c3ad
AmlMoawadElshora <ineedtorun127@gmail.com>
2024-03-08 12:54:45 +0200

    files upload

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, jsonify, render_template
 import numpy as np
 import tensorflow as tf
 from colorthief import ColorThief
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -35,6 +37,31 @@ def predict():
         result = categories[np.argmax(predictions)]
 
         return jsonify({'result': result})
+
+@app.route('/predictApi', methods=["POST"])
+def api():
+    try:
+        if 'file' not in request.files:
+            return jsonify({'error': "No file part in the request"})
+        file = request.files['file']
+
+        if file.filename == '':
+            return jsonify({'error': "No selected file"})
+        
+        img = file.stream
+        color_thief = ColorThief(img)
+        dominant_color = color_thief.get_color(quality=1)
+
+        # Predict using the model
+        predictions = model.predict(np.array([dominant_color]))
+
+        # Map predictions to categories
+        categories = ['Red', 'Green', 'Blue', 'Yellow', 'Orange', 'Pink', 'Purple', 'Brown', 'Grey', 'Black', 'White']
+        predicted_color = categories[np.argmax(predictions)]
+
+        return jsonify({'predicted_color': predicted_color})
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
